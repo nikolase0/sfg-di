28f0c3379bf3c9768df0fc38ad0f85364308421d
nikolase0 <serafimovic28@gmail.com>
2022-02-07 23:18:42 +0100

    Initial commit

---
+++ src/test/java/ns/springframework/sfgdi/SfgDiApplicationTests.java
@@ -0,0 +1,13 @@
+package ns.springframework.sfgdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
